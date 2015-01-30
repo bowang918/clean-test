775f4451ec52d9d084ed3dd4ef2d7db85f6545ae
Wang Bo <nanfengbobo@gmail.com>
2015-01-30 18:00:12 +0800

    Create DuplicateEmailException.java

---
+++ src/main/java/com/seven7/starter/security/exception/DuplicateEmailException.java
@@ -0,0 +1,14 @@
+package com.seven7.starter.security.exception;
+
+public class DuplicateEmailException extends RuntimeException {
+
+	public DuplicateEmailException(String string) {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 2219915855132635553L;
+
+}
